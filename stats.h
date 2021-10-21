b7906ac4a249d77f018f375b7193837b31876dde
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2021-10-21 18:13:40 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,3 +1,6 @@
+#ifndef STATS_H 
+#define STATS_H 
+
 #include <math.h>
 #ifdef NAN
 /* NAN is supported */
@@ -15,5 +18,5 @@ extern  Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)();
 extern void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
-
+ #endif
 
