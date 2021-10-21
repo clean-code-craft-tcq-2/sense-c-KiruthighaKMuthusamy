dc8836a7a77697f430b28a1eb1e1a3a4c34b4065
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2021-10-21 18:15:58 +0530

    Update count.c

--- count.c
+++ count.c
@@ -1,14 +1,2 @@
-#include "stats.h"
-#include "count.h"
 
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
 
-void emailAlerter()
-{	
-	 emailAlertCallCount = 1;
-}
-void ledAlerter()
-{
- ledAlertCallCount = 1;
-}
