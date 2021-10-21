954435acd258e0c9971eced0fcef21e415015825
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2021-10-21 17:46:09 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -15,5 +15,5 @@ extern  Stats compute_statistics(const float* numberset, int setlength);
 typedef void (*alerter_funcptr)();
 extern void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
+
+
