bcf562b4c691fe4bd10daaf8d4a97c3fbfe85710
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2021-10-22 19:19:49 +0530

    Update count.h

--- count.h
+++ count.h
@@ -1,6 +1,8 @@
 #ifndef COUNT_H 
 #define COUNT_H 
 
+#include "stats.h"
+
 typedef void (*alerter_funcptr)();
 
 extern void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
