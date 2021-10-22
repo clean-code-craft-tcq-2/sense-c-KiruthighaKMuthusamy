4fab73f4812fc40632fb2bd9d75136c960e224a7
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2021-10-22 19:10:51 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 #include "stats.h"
 
-int emailAlertCallCount = 0;
-int ledAlertCallCount = 0;
-Stats compute_statistics(const float* numberset, int setlength) {
+//Testcase::("reports average, minimum and maximum")
+Stats compute_statistics(const float* numberset, int setlength) 
+{
 	Stats s;
 	s.average = 0;
     s.min = numberset[0];
@@ -13,61 +13,45 @@ Stats compute_statistics(const float* numberset, int setlength) {
 	
 	for (int i=0;i<setlength;i++)
 	{
-	total+=	numberset[i] ;
-	if (s.min > numberset[i])
-	{
-		s.min = numberset[i];
+		total+=	numberset[i] ;
+		if (s.min > numberset[i])
+		{
+			s.min = numberset[i];
+		}
+		if (s.max < numberset[i])
+		{
+			s.max = numberset[i];
 	}
-	if (s.max < numberset[i])
-	{
-		s.max = numberset[i];
-	}
-	}
-	//Testcase2:
-	if ((setlength == 0 )|| (total == 0.0))
+		
+	//Testcase:"average is NaN for empty array"
+	if ((setlength == 0 ))
 	{
 		
-   s.average = NAN;
-	s.min = NAN;
-  s.max = NAN;
-
+       s.average = NAN;
+	   s.min = NAN;
+       s.max = NAN;
 		
 	}
-else
-{
-	s.average = total / setlength;
+	else
+	{
+	   s.average = total / setlength;
+	}
+	
+return s;
 }
-return s;}
 
-Stats compute_statistics(const int numberset, int setlength) {
-if ((setlength == 0 ) || (numberset == 0))
+//Testcase:"average is NaN for empty array"
+Stats compute_statistics(const int numberset, int setlength)
+ {
+	 
+	if ((setlength == 0 ) || (numberset == 0))
 	{
 		Stats s;
-   s.average = NAN;
-    s.min = NAN;
-    s.max = NAN;
+		s.average = NAN;
+		s.min = NAN;
+		s.max = NAN;
 		return s;		
 	}
 	
 }
-
-void emailAlerter()
-{	
-	 emailAlertCallCount = 1;
-}
-void ledAlerter()
-{
- ledAlertCallCount = 1;
-}
-
-void check_and_alert(float maxThreshold,alerter_funcptr alerters[], Stats computedStats)
-{
-	if(computedStats.max > maxThreshold)
-	{
-		alerters[0]();
-		alerters[1]();
-	}
-	
-}
-
 
