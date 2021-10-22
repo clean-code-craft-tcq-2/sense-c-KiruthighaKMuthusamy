c84072615dddcd0ccc2da80af12c721deaf214f6
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2021-10-22 19:11:49 +0530

    Update count.c

--- count.c
+++ count.c
@@ -1,2 +1,23 @@
+#include "count.h"
 
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
 
+void emailAlerter()
+{	
+	 emailAlertCallCount = 1;
+}
+void ledAlerter()
+{
+ ledAlertCallCount = 1;
+}
+
+void check_and_alert(float maxThreshold,alerter_funcptr alerters[], Stats computedStats)
+{
+	if(computedStats.max > maxThreshold)
+	{
+		alerters[0]();
+		alerters[1]();
+	}
+	
+}
