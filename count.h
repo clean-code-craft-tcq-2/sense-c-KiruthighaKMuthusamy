d5af89aa0fadb11cafb8beefada79025e7871914
KiruthighaKMuthusamy <91048874+KiruthighaKMuthusamy@users.noreply.github.com>
2021-10-22 19:10:28 +0530

    Update count.h

--- count.h
+++ count.h
@@ -1,4 +1,14 @@
+#ifndef COUNT_H 
+#define COUNT_H 
 
+typedef void (*alerter_funcptr)();
 
+extern void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
+extern void  emailAlerter();
+extern void  ledAlerter();
 
+extern int emailAlertCallCount;
+extern int ledAlertCallCount;
+
+#endif
 
